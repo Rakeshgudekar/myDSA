d9df17244de989e1f76ad65a860c53dba49ba982
RakeshGudekar <rakeshgudekar100@gmail.com>
2022-10-05 15:17:48 +0530

    sum/kadane's algo

--- src/Maximum_subArray.java
+++ src/Maximum_subArray.java
@@ -1,2 +1,21 @@
 public class Maximum_subArray {
+
+
+    public static void main(String[] args) {
+    int[] arr={1,2,-2,4,-5,3,4,-2,3};
+    int ans=sum(arr);
+        System.out.println(ans);
+    }
+    // kadane's algorithm
+   static int sum(int[]arr){
+        int sum=0;
+        int max=arr[0];
+       for (int i = 0; i < arr.length; i++) {
+           sum+=arr[i];
+           if(sum>max) max=sum;
+           if(sum<0) sum=0;
+       }
+       return max;
+   }
+
 }
